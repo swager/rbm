9f1f6d83f2f00018e0cae515224707a15102eaa6
Léo Belzile <belzilel@gmail.com>
2023-06-22 07:13:33 -0400

    Define sample size

--- R/hill.R
+++ R/hill.R
@@ -7,6 +7,7 @@
 #' @export hill
 hill = function(data_array, idx = 1:(length(logsorted) - 1)) {
   useable_data = data_array[data_array > 0];
+  n <- length(useable_data)
   logsorted = log(sort(useable_data));
   gammaH = rep(0, length(idx));
   for (iter in 1:length(idx))
